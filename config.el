4a28bf516b5c5ffbc689a183796b9730ec931eb7
gcclll <gccll.love@gmail.com>
2021-07-22 23:38:53 +0800

    push

--- config.el
+++ config.el
@@ -249,8 +249,9 @@ Finally save buffer.
   (load custom-file))
 
 (global-set-key (kbd "s-p") nil)        ; ns-print-buffer
-;; (global-set-key (kbd ",") nil)
-(map! "C-e" nil)
+(global-set-key (kbd "<f2>") nil)        ; ns-print-buffer
+(map! "C-e" nil
+      :n        ","     nil)
 
 (global-set-key (kbd "<f3>") 'hydra-multiple-cursors/body)
 (global-set-key (kbd "<f5>") 'deadgrep)
@@ -272,7 +273,7 @@ Finally save buffer.
  "C-a"          #'crux-move-beginning-of-line
  "C-s"          #'+default/search-buffer
  "C-:"          #'avy-goto-char
- "C-;"          #'avy-goto-char-2
+ ;; "C-;"          #'avy-goto-char-2
  ;; "C-`"          #'popper-toggle-latest
  ;; "C-~"          #'popper-cycle
 
@@ -497,6 +498,46 @@ Finally save buffer.
   :config
   (add-hook! 'after-init-hook #'global-hungry-delete-mode)
   (global-hungry-delete-mode 1))
+
+(defhydra gcl-jump (:color blue :columns 3 :hint nil)
+  "Jump ->"
+  ("c" gcl-jump-char/body "Avy Char Jump")
+  ("l" gcl-jump-line/body "Avy Line Jump")
+  ("w" gcl-jump-word/body "Avy Word Jump")
+  )
+(global-set-key (kbd "<f2>") 'gcl-jump/body)        ; ns-print-buffer
+
+(defhydra gcl-jump-char (:color blue :columns 3 :hint nil)
+  "Jump By Char ->"
+  ("c" avy-goto-char "Char")
+  ("l" avy-goto-char-in-line "In line")
+  ("t" avy-goto-char-timer "Timer")
+  ("2" avy-goto-char-2 "Char2")
+  ("a" avy-goto-char-2-above "Above")
+  ("b" avy-goto-char-2-below "Below")
+  )
+
+(defhydra gcl-jump-line (:color blue :columns 3 :hint nil)
+  "Jump To Line ->"
+  ("u" avy-goto-line-above "Above")
+  ("d" avy-goto-line-below "Below")
+  ("s" avy-goto-line "Line Start")
+  ("e" avy-goto-end-of-line "Line End")
+  )
+
+(defhydra gcl-jump-word (:color blue :columns 3 :hint nil)
+  "Jump By Word ->"
+  ("l" avy-jump-to-word-in-line "in line")
+  ("w" avy-goto-word-1 "word1")
+  ("0" avy-goto-word-0 "word0")
+  ("a" avy-goto-word-0-above "above-0")
+  ("A" avy-goto-word-1-above "above-1")
+  ("b" avy-goto-word-0-below "below0")
+  ("B" avy-goto-word-1-below "below1")
+  ("o" avy-goto-word-or-subword-1 "word or subword")
+  ("s" avy-subword-0 "subword-0")
+  ("S" avy-subword-1 "subword-1")
+  )
 
 (global-set-key (kbd "C-'") 'imenu-list-smart-toggle)
 
@@ -1271,10 +1312,12 @@ is selected, only the bare key is returned."
   (popper-reference-buffers
    '("*eshell*"
      "*vterm*"
+     "*color-rg*"
      "Output\\*$"
      "*Process List*"
      "COMMIT_EDITMSG"
      embark-collect-mode
+     deadgrep-mode
      grep-mode
      rg-mode
      rspec-compilation-mode
