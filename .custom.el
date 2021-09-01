4bf6f2b2661f5a728fd22dd306c6365a2c9c8a18
gcclll <gccll.love@gmail.com>
2021-09-01 17:54:59 +0800

    2021-09-01 17:54:59

--- .custom.el
+++ .custom.el
@@ -19,7 +19,7 @@
  '(jdee-db-requested-breakpoint-face-colors (cons "#1c1f24" "#7bc275"))
  '(jdee-db-spec-breakpoint-face-colors (cons "#1c1f24" "#484854"))
  '(objed-cursor-color "#ff665c")
- '(package-selected-packages '(google exec-path-from-shell devdocs))
+ '(package-selected-packages '(jq-format google exec-path-from-shell devdocs))
  '(pdf-view-midnight-colors (cons "#bbc2cf" "#242730"))
  '(rustic-ansi-faces
    ["#242730" "#ff665c" "#7bc275" "#FCCE7B" "#51afef" "#C57BDB" "#5cEfFF" "#bbc2cf"])
